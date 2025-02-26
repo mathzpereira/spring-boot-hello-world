bcdf65d4f6a1e5642606864ab601e4f08cfb9a76
Mathz <mathlimapereira@gmail.com>
2025-02-25 22:07:53 -0300

    initial commit

---
+++ app/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/hulk")
+	public String hulk(){
+		return "Hulk é o super-herói alvinegro";
+	}
+
+}
